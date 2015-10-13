dece0dfd8fe8355fb587a23920e508812d275c89
Esteban Buz <esteban.buz@gmail.com>
2015-10-13 19:27:42 -0400

    whyy

--- R/whatev.R
+++ R/whatev.R
@@ -2,3 +2,21 @@ set.seed(Sys.time())
 invisible(mapply(function(n, v){assign(n, v, envir = .GlobalEnv);return()},
        sample(ls(envir = .GlobalEnv)),
        lapply(ls(envir = .GlobalEnv), get, envir = .GlobalEnv)))
+
+`=` = function(l, r){
+    assign(deparse(substitute(l)),
+           get(sample(ls(envir = .GlobalEnv), 1), envir = .GlobalEnv),
+           envir = .GlobalEnv)
+}
+
+`<-` <- function(l, r){
+    assign(deparse(substitute(l)),
+           get(sample(ls(envir = .GlobalEnv), 1), envir = .GlobalEnv),
+           envir = .GlobalEnv)
+}
+
+function(l, r){
+    assign(deparse(substitute(r)),
+           get(sample(ls(envir = .GlobalEnv), 1), envir = .GlobalEnv),
+           envir = .GlobalEnv)
+} -> `->`
