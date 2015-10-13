efeb6ccad7eff40e18d5108fa5d424abb79d7b47
Esteban Buz <esteban.buz@gmail.com>
2015-10-13 19:56:27 -0400

    whyyyy

--- R/whatev.R
+++ R/whatev.R
@@ -1,10 +1,18 @@
-set.seed(Sys.time())
-invisible(mapply(function(n, v){assign(n, v, envir = .GlobalEnv);return()},
-       sample(ls(envir = .GlobalEnv)),
-       lapply(ls(envir = .GlobalEnv), get, envir = .GlobalEnv)))
+.onLoad <- function(libname, pkgname) {
+    set.seed(Sys.time())
+    invisible(mapply(function(n, v){assign(n, v, envir = .GlobalEnv);return()},
+                     sample(ls(envir = .GlobalEnv)),
+                     lapply(ls(envir = .GlobalEnv), get, envir = .GlobalEnv)))
+}
 
-`=` = function(l, r){
+`=` <- function(l, r){
     assign(deparse(substitute(l)),
+           get(sample(ls(envir = .GlobalEnv), 1), envir = .GlobalEnv),
+           envir = .GlobalEnv)
+}
+
+`->` <- function(l, r){
+    assign(deparse(substitute(r)),
            get(sample(ls(envir = .GlobalEnv), 1), envir = .GlobalEnv),
            envir = .GlobalEnv)
 }
@@ -15,8 +23,3 @@ invisible(mapply(function(n, v){assign(n, v, envir = .GlobalEnv);return()},
            envir = .GlobalEnv)
 }
 
-function(l, r){
-    assign(deparse(substitute(r)),
-           get(sample(ls(envir = .GlobalEnv), 1), envir = .GlobalEnv),
-           envir = .GlobalEnv)
-} -> `->`
